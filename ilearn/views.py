494e754de5474e6b250e4c31d289238449ab46a0
Mayowa Emmanuel Olabanji <mayowae@outlook.com>
2021-04-19 14:18:51 +0100

    Setup Django

--- ilearn/views.py
+++ ilearn/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse('''
+    <h1>Hello Zuri!</h1>
+    <p>
+    My Name is Olabanji MAyowa Emmanuel
+    I'm learning python with django with Zuri & I4G
+    </p>
+    ''')
